0f58e7aab6de42b4b8f36c567b677e1c95dfab74
Rory Finnegan <bunnehrealm@gmail.com>
2024-08-29 19:07:54 -0400

    Add multiple entries. Add Headers
    Still sub-version 1.0

--- deployme.go
+++ deployme.go
@@ -3,11 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
 	"github.com/xuri/excelize/v2"
 )
+
+func findStart(f *excelize.File) int {
+	for row := 1; ; row++ {
+		// Get the value from column A for the current row
+		cell, err := f.GetCellValue("Sheet1", fmt.Sprintf("A%d", row))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Check if the cell is empty
+		if cell == "" {
+			return row
+		}
+
+	}
+
+}
 
 func main() {
 
@@ -21,8 +39,14 @@ func main() {
 		if err != nil {
 			fmt.Println("Could not make columns wide :(")
 		}
+
+		f.SetCellValue("Sheet1", "A1", "ASSET TAG")
+		f.SetCellValue("Sheet1", "B1", "SERIAL")
+		f.SetCellValue("Sheet1", "C1", "TECHNICIAN")
+		f.SetCellValue("Sheet1", "D1", "TIME")
 	}
 
+	count := findStart(f)
 	scanner := bufio.NewScanner(os.Stdin)
 	var asset, serial, user string
 	for {
@@ -51,14 +75,15 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 		}
 
-		f.SetCellValue("Sheet1", "A1", asset)
-		f.SetCellValue("Sheet1", "B1", serial)
-		f.SetCellValue("Sheet1", "C1", user)
-		f.SetCellValue("Sheet1", "D1", time.Now())
+		f.SetCellValue("Sheet1", fmt.Sprintf("A%d", count), asset)
+		f.SetCellValue("Sheet1", fmt.Sprintf("B%d", count), serial)
+		f.SetCellValue("Sheet1", fmt.Sprintf("C%d", count), user)
+		f.SetCellValue("Sheet1", fmt.Sprintf("D%d", count), time.Now())
 
 		if err := f.SaveAs("deploymebook.xlsx"); err != nil {
 			fmt.Println(err)
 		}
+		count++
 	}
 
 }
